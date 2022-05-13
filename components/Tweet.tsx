a321b860438967e8d9727a276a662e3368150e3c
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2022-05-13 13:23:15 +0530

    finished darkmode

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -120,7 +120,7 @@ function Tweet({ tweet }: Props) {
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
+            className="flex-1 rounded-xl bg-transparent/20 p-2 text-white outline-none"
             type="text"
             placeholder="Write a comment..."
           />
